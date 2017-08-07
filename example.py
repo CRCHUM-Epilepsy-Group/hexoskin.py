44256be8f40b9d9e212af1477c9a265b065558e8
Rob Linton <roblinton@gmail.com>
2017-08-07 15:55:52 -0400

    Fixed bugs in the example script.  Added a raw data download example.

--- example.py
+++ example.py
@@ -6,71 +6,117 @@ import hexoskin.errors
 # Example of setting a new cache file.
 # hexoskin.client.CACHED_API_RESOURCE_LIST = '.new_file'
 
-try:
-
-    # Initialize the API object.
-    conf = {
-        'api_key': '[your key]',
-        'api_secret': '[your secret]',
-        'user_auth': 'user@example.com:passwd',
-        'api_version': '2.0',
-    }
-    api = hexoskin.client.HexoApi(**conf)
-
-    # Get the current user's info
-    user = api.account.list()[0]
-    print user
-
-    # # All the users you can see:
-    users = api.user.list()
-    print users[0]
+# Initialize the API object.
+conf = {
+    # 'api_key': '[your key]',
+    # 'api_secret': '[your secret]',
+    # 'user_auth': 'user@example.com:passwd',
+    'api_key': 'aBt99STtaKaZbxkdc4gyB5vleepApA',
+    'api_secret': 'z3SRgJPaaCSwTfhRxZDbDRoJ78kzyN',
+    'auth': 'rob.linton@carretechnologies.com:asdfasdf',
+    # 'api_version': 'latest',
+}
+api = hexoskin.client.HexoApi(**conf)
 
 
-    # Get a list of resources, datatype for instance.
-    datatypes = api.datatype.list()
-
-    # `datatypes` is a ApiResourceList of ApiResourceInstances.  You can
-    # `access it like a list:
-    print datatypes[0]
-
-    # You can get the next page.  Now datatypes is 40 items long.
-    datatypes.load_next()
-
-    # You can delete right from the list!  This would send a delete request to
-    # the API except it's not allowed.
+def basic_test():
+    """Runs through the some basic API operations."""
     try:
-        del datatypes[5]
+        # Get the current user's info
+        user = api.account.list()[0]
+        print user
+
+        # # All the users you can see:
+        users = api.user.list()
+        print users[0]
+
+        # Get a list of resources, datatype for instance.
+        datatypes = api.datatype.list()
+
+        # `datatypes` is a ApiResourceList of ApiResourceInstances.  You can
+        # `access it like a list:
+        print datatypes[0]
+
+        # You can get the next page.  Now datatypes is 40 items long.
+        datatypes.load_next()
+
+        # You can delete right from the list!  This would send a delete request to
+        # the API except it's not allowed.
+        try:
+            del datatypes[5]
+        except hexoskin.errors.MethodNotAllowed, e:
+            print "Oh no you di'nt! %s" % e
+
+        # You can create items, a Range for instance:
+        new_range = api.range.create({'name':'testnew_range', 'start':353163129199, 'end':353163139199, 'user':user.resource_uri})
+
+        # `new_range` is an ApiResourceInstance.  You can modify it in place:
+        new_range.name = 'newtestyrangey'
+
+        # And update the server:
+        new_range.update()
+        print new_range
+
+        # Or by passing a dictionary to update(), note how I can use an
+        # ApiResourceInstance as a value here.  That works with the assignment
+        # method above too:
+        new_range.update({'user': users[0]})
+        print new_range
+
+        # And of course, delete it:
+        new_range.delete()
+
+    except hexoskin.errors.HttpError, e:
+        # All HttpErrors have an ApiResponse object in `response`.  The string
+        # representation includes the body so can be quite large but it is often
+        # useful.
+        print e.response
+
     except hexoskin.errors.MethodNotAllowed, e:
-        print "Oh no you di'nt! %s" % e
-
-    # You can create items, a Range for instance:
-    new_range = api.range.create({'name':'testnew_range', 'start':353163129199, 'end':353163139199, 'user':user.resource_uri})
-
-    # `new_range` is an ApiResourceInstance.  You can modify it in place:
-    new_range.name = 'newtestyrangey'
-
-    # And update the server:
-    new_range.update()
-    print new_range
-
-    # Or by passing a dictionary to update(), note how I can use an
-    # ApiResourceInstance as a value here.  That works with the assignment
-    # method above too:
-    new_range.update({'user': users[0]})
-    print new_range
-
-    # And of course, delete it:
-    new_range.delete()
+        # Requests are verified client-side before being sent.  If you try to use
+        # a method that's not allowed, a MethodNotAllowed exception is raised.
+        print "You can't do that! %s" % e
 
 
+class DataPoller(object):
+    """An example of an approach for polling for realtime data in a cache-
+    friendly fashon."""
+
+    def __init__(self, api, datatypes, **kwargs):
+        self.since = 0
+        self.window = 256*60*10
+        self.api = api
+        self.datatypes = datatypes
+        self.filter_args = kwargs
+
+    def poll(self):
+        now = int(time.mktime(datetime.datetime.now().timetuple())) * 256
+        if now - self.since > self.window:
+            self.since = now
+        self.filter_args.update({'start': self.since, 'end': self.since+self.window})
+        result = self.api.data.list(datatype__in=self.datatypes, **self.filter_args)
+        if result:
+            self.since = max([max(v)[0] for d,v in result[0].data.items()])
+            if len(result[0].data.itervalues().next()) > 1:
+                return result[0].data
+        return []
 
 
-except hexoskin.errors.HttpError, e:
-    # All HttpErrors have an ApiResponse object in `response`.  The string
-    # representation includes the body so can be quite large but it is often
-    # useful.
-    print e.response
-except hexoskin.errors.MethodNotAllowed, e:
-    # Requests are verified client-side before being sent.  If you try to use
-    # a method that's not allowed, a MethodNotAllowed exception is raised.
-    print "You can't do that! %s" % e
+def download_raw(**kwargs):
+    """An example of downloading raw data and saving it to disk.
+
+    \param kwargs The arguments to determine the data.  Expected to be record=12345 or
+        range=12345 for sane filenames.
+    """
+    # qry = sys.argv[1]
+    formats = {
+        'edf': 'application/x-edf',
+        'zip': 'application/octet-stream',
+    }
+    fmt = kwargs.pop('format', 'edf').lower()
+    mimetype = formats[fmt]
+    fname = '{}.{}'.format('_'.join('{}_{}'.format(k,v) for k,v in kwargs.items()), fmt)
+    api.oauth2_get_access_token(*conf['auth'].split(':', 1))
+    with open(fname, 'wb') as f:
+        f.write(api.data.list(kwargs, 'application/x-edf'))
+    print "File written as {}".format(fname)
