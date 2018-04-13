e05131fbc612a307f5b01c59ed539af3c45ceb48
Rob Linton <roblinton@gmail.com>
2018-04-13 14:38:45 -0400

    Made the HttpError object handle JSON errors a little more nicely.

--- hexoskin/errors.py
+++ hexoskin/errors.py
@@ -4,10 +4,19 @@ class ApiError(Exception):
 class MethodNotAllowed(Exception):
     pass
 
+
 class HttpError(Exception):
+
     def __init__(self, response=None):
         if response is not None:
             self.response = response
+
+    def __str__(self):
+        try:
+            return str(self.response.json()['errors'])
+        except:
+            return super(HttpError, self).__str__()
+
 
 class HttpClientError(HttpError):
     pass
